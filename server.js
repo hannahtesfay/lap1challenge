820e768ff24308e3927bc82e18da4b993bc83ef3
hannahtesfay <hannahltesfay@gmail.com>
2020-07-22 11:21:45 +0100

    make separate arrays for titles and links

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  const port = 3000;
  server.use(cors());
 
- const animalSearch = [
+ const animals = [
      "Giraffe - https://en.wikipedia.org/wiki/Giraffe",
      "Fox - https://en.wikipedia.org/wiki/Fox",
      "Elephant - https://en.wikipedia.org/wiki/Elephant",
@@ -12,11 +12,20 @@
      "Zebra - https://en.wikipedia.org/wiki/Zebra",
  ]
 
+ const animalLinks = [
+    "Giraffe - https://en.wikipedia.org/wiki/Giraffe",
+    "Fox - https://en.wikipedia.org/wiki/Fox",
+    "Elephant - https://en.wikipedia.org/wiki/Elephant",
+    "Gazelle - https://en.wikipedia.org/wiki/Gazelle",
+    "Zebra - https://en.wikipedia.org/wiki/Zebra",
+]
+
  // response at root page
  server.get(`/`, (req, res) => res.send('hello world'));
 
  // response when searching
- server.get(`/search`, (req, res) => res.send(JSON.stringify(animalSearch)))
+ server.get(`/search`, (req, res) => res.send(JSON.stringify(animals)))
+ server.get(`/links`, (req, res) => res.send(JSON.stringify(animalsLinks)))
 
  // response for I'm feeling lucky
  server.get(`/lucky`, (req, res) => res.send(JSON.stringify(animalSearch[(Math.floor(Math.random()*5))])))
